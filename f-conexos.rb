b77cb704ab6eb7cc93db17d9edb2dfd54d9d26a0
João Luna <joaoluna@edu.unifor.br>
2022-10-05 23:07:46 -0300

    grafo teste -> componentes fortemente conectadas

--- f-conexos.rb
+++ f-conexos.rb
@@ -35,26 +35,6 @@ def componentes_f_conexos
     grafo.addAresta(g,f)
     grafo.addAresta(g,h)
     grafo.addAresta(h,h)
-=begin
-    puts "Grafo de tamanho: #{grafo.size}"
-    puts "Exibindo o vértice C"
-    grafo.getVertice("c").printV
-    puts "Exibindo vizinhos de C"
-    grafo.getVertice("c").printVizinhos
-    puts
-    puts "<--------- Usando DFS --------->"
-    grafo.dfs
-    puts "Exibindo o vertice qualquer após o DFS: "
-    grafo.getVertice("a").printV
-    grafo.getVertice("b").printV
-    grafo.getVertice("c").printV
-    grafo.getVertice("d").printV
-    grafo.getVertice("e").printV
-    grafo.getVertice("f").printV
-    grafo.getVertice("g").printV
-    grafo.getVertice("h").printV
-    puts
-=end
 
     grafo.print_cfc
     puts puts
@@ -90,31 +70,11 @@ def caso_teorico
     grafo.addAresta(w,z)
     grafo.addAresta(z,z)
 
-=begin
-    puts "Grafo de #{grafo.size} vertices!"
-    puts "Exibindo o vertice U:"
-    grafo.getVertice("u").printV
-    puts "Exibindo os vizinhos de U:"
-    grafo.getVertice("u").printVizinhos
-
-    puts "<--------- Usando DFS --------->"
-    grafo.dfs
-    puts "Exibindo o vertice qualquer após o DFS: "
-    grafo.getVertice("v").printV
-
-
-    grafo.printG
-    puts
-    puts
-    puts "Transposta gt"
-    gt = grafo.getTranspose
-    gt.printG
-=end
     grafo.print_cfc
 
 end
 
-def componentes_f_conexos
+def teste
     grafo = Grafo.new
     a = Vertice.new "a"
     b = Vertice.new "b"
@@ -148,6 +108,6 @@ def componentes_f_conexos
     grafo.recomendacao("a")
 end
 
-
-componentes_f_conexos
+teste
+#componentes_f_conexos
 #caso_teorico
